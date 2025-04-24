e370d3fb9137cc4026908994f22efcecf54bb8f7
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2025-04-24 11:25:18 +0400

    app config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        PORT: Joi.number().integer(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        user: configService.get<string>('MONGODB_USER'),
+        pass: configService.get<string>('MONGODB_PASS'),
+        dbName: configService.get<string>('MONGODB_DB'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
