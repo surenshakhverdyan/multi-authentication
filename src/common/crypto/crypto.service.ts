7e4bb8183474f77a42e86c4ec64dcdf48b571780
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2025-04-24 16:09:30 +0400

    bug fix

--- src/common/crypto/crypto.service.ts
+++ src/common/crypto/crypto.service.ts
@@ -7,15 +7,12 @@ export class CryptoService {
   private saltRounds = 10;
   private salt: string;
 
-  constructor() {
-    this.salt = bcrypt.genSaltSync(this.saltRounds);
-  }
-
   async hashPassword(password: string): Promise<string> {
-    return bcrypt.hash(password, this.salt);
+    this.salt = await bcrypt.genSalt(this.saltRounds);
+    return await bcrypt.hash(password, this.salt);
   }
 
   async comparePassword(password: string, hash: string): Promise<boolean> {
-    return bcrypt.compare(password, hash);
+    return await bcrypt.compare(password, hash);
   }
 }
