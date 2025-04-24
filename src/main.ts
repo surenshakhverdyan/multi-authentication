5bdc8b8bca387c7dd95e40113c34f0a86e0ca9da
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2025-04-24 11:30:20 +0400

    app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get<ConfigService>(ConfigService);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN'),
+  });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 void bootstrap();
